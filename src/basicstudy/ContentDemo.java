00100ea230f98da1b855e38540d2d74ce77689ff
官华金 <1414122327@qq.com>
2021-01-24 16:20:20 +0800

    初始化code

---
+++ src/basicstudy/ContentDemo.java
@@ -0,0 +1,31 @@
+package basicstudy;
+
+public class ContentDemo {
+    /*
+    快捷方式：
+        main方法的快捷键： psvm
+        输出语句的快捷键： sout
+
+    常量：
+      概念：不变化的量，即不会被人为修改的量
+      常量的分类：
+       1。字面值常量（掌握）
+        字符串常量  "HelloWorld"
+        整数常量    12  -23
+        小数常量    12.34
+        字符常量    'a'
+        布尔常量     true,false
+        空常量       null
+      2。自定义常量（了解）
+      注意事项：区分字符串常量和字符常量
+
+
+
+
+
+
+     */
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
